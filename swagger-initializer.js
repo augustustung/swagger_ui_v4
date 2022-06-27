2806109db8048a351a7bcf453558408024340d1d
augustustung <huytung139@gmail.com>
2022-06-27 11:26:38 +0700

    prompt url

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -1,9 +1,9 @@
-window.onload = function() {
+window.onload = function () {
   //<editor-fold desc="Changeable Configuration Block">
-
+  let url = window.prompt("What is the URL ?");
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: url,
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
